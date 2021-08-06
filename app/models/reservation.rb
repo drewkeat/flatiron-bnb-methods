afd0fd12465b588d9e5b3f9f8a54e223b602c49a
Drew Keat <thedrewkeat@gmail.com>
2021-08-05 21:57:51 -0600

    complete reservation instance methods

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,4 +2,12 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
+
+  def duration
+    (self.checkout - self.checkin).to_i
+  end
+
+  def total_price
+    self.listing.price * self.duration
+  end
 end
