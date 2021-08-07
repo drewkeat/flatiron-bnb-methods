d5488304f20bd8389c92e2313fddbe026a5b812d
Drew Keat <thedrewkeat@gmail.com>
2021-08-07 10:00:00 -0600

    validate reviews after trip ends and reservation accepted, user as host knows about guests

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,19 @@ class Review < ActiveRecord::Base
   belongs_to :guest, :class_name => "User"
 
   validates :rating, :description, :reservation_id, presence: :true
+  validate :trip_ended, :reservation_accepted
 
+  private
+  
+    def trip_ended
+      unless self.reservation && self.reservation.checkout < Date.today
+        errors.add(:reservation_id, "Stay must be complete before leaving a review.")
+      end
+    end
+
+    def reservation_accepted
+      unless self.reservation && self.reservation.status == "accepted"
+        errors.add(:reservation_id, "This reservation was not accepted.")
+      end
+    end
 end
