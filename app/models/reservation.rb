2f5a780d4f5e62a315ff75c249805a929c691a8a
Drew Keat <thedrewkeat@gmail.com>
2021-08-07 07:49:32 -0600

    reservation validates checkout after checkin

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -2,8 +2,10 @@ class Reservation < ActiveRecord::Base
   belongs_to :listing
   belongs_to :guest, :class_name => "User"
   has_one :review
-
+  
+  validate :checkout_after_checkin
   validates :checkin, :checkout, presence: true
+
 
   def duration
     (self.checkout - self.checkin).to_i
@@ -12,4 +14,15 @@ class Reservation < ActiveRecord::Base
   def total_price
     self.listing.price * self.duration
   end
+
+  private
+
+    def checkout_after_checkin
+      return if checkout.blank? || checkin.blank?
+      
+      if checkout < checkin || checkout == checkin
+        errors.add(:checkout, "must be after the checkin") 
+      end 
+    end
+
 end
